1ac9d87d8dfa8012b7cc9ec50daf1150176db35e
msj56@hotmail.com <msj56@hotmail.com>
2023-01-31 16:43:56 -0300

    Initial commit

---
+++ src/test/java/br/com/consorcio/ConsorcioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consorcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsorcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
